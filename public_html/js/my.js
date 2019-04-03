fc1cb22042cdb44f9db082a860bb79af1934a4e7
Dmitry Novikov <nerosketch@gmail.com>
2019-04-03 16:22:28 +0300

    оформил доку по установке Djing

--- public_html/js/my.js
+++ public_html/js/my.js
@@ -9,35 +9,37 @@ document.querySelector('.btn_grad').onmousemove = function (e) {
 
 // Line
 let c = document.getElementById('canv');
-let $ = c.getContext('2d');
-let w = c.width = document.body.clientWidth;
-let h = c.height = 541;
-let draw = function (a, b, t) {
-    $.lineWidth = 0.8;
-    $.fillStyle = 'hsl(0, 0%, 0%)';
-    $.fillRect(0, 0, w, h);
-    for (let i = -60; i < 60; i += 1) {
-        $.strokeStyle = 'hsla(0, 0%, 46%, 0.03)';
-        $.beginPath();
-        $.moveTo(0, h / 2);
-        for (let j = 0; j < w; j += 10) {
-            $.lineTo(10 * Math.sin(i / 10) + j + 0.008 * j * j, Math.floor(
-                h / 2 + j / 2 * Math.sin(j / 50 - t / 50 - i / 118) +
-                (i * 0.9) * Math.sin(j / 25 - (i + t) / 65)));
+if(c) {
+    let $ = c.getContext('2d');
+    let w = c.width = document.body.clientWidth;
+    let h = c.height = 541;
+    let draw = function (a, b, t) {
+        $.lineWidth = 0.8;
+        $.fillStyle = 'hsl(0, 0%, 0%)';
+        $.fillRect(0, 0, w, h);
+        for (let i = -60; i < 60; i += 1) {
+            $.strokeStyle = 'hsla(0, 0%, 46%, 0.03)';
+            $.beginPath();
+            $.moveTo(0, h / 2);
+            for (let j = 0; j < w; j += 10) {
+                $.lineTo(10 * Math.sin(i / 10) + j + 0.008 * j * j, Math.floor(
+                    h / 2 + j / 2 * Math.sin(j / 50 - t / 50 - i / 118) +
+                    (i * 0.9) * Math.sin(j / 25 - (i + t) / 65)));
+            }
+            $.stroke();
         }
-        $.stroke();
-    }
-};
-let t = 0;
-window.addEventListener('resize', function () {
-    c.width = w = document.body.clientWidth;
-    c.height = h = 541;
-    $.fillStyle = 'hsl(0, 0%, 0%)';
-}, false);
-let run = function () {
-    window.requestAnimationFrame(run);
-    t += 5;
-    draw(33, 52 * Math.sin(t / 2400), t);
-};
-run();
+    };
+    let t = 0;
+    window.addEventListener('resize', function () {
+        c.width = w = document.body.clientWidth;
+        c.height = h = 541;
+        $.fillStyle = 'hsl(0, 0%, 0%)';
+    }, false);
+    let run = function () {
+        window.requestAnimationFrame(run);
+        t += 5;
+        draw(33, 52 * Math.sin(t / 2400), t);
+    };
+    run();
+}
 // End Line
